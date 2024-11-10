b213c1bda82f0b53f65a4b0c46d36428a42b570d
Roberto <angeldiablitotkd66@gmail.com>
2024-11-10 05:36:24 -0500

    Segundo commit-creación de otra carpeta-creación de un código en C++

---
+++ Ordenamiento/Ejercicio 1.cpp
@@ -0,0 +1,15 @@
+// 1. Ordenamiento por selección
+
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main () {
+	
+	cout << "Aun no se resolvio, solo es una prueba";
+	
+	system("pause");
+	
+	return 0;
+}
