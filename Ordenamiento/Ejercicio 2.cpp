56c8ac6d9e8d43dcea5b5381b2acb66e6f773446
Roberto <angeldiablitotkd66@gmail.com>
2024-11-10 05:52:14 -0500

    Cuarto commit-creación ejercicio 2 en C++

---
+++ Ordenamiento/Ejercicio 2.cpp
@@ -0,0 +1,12 @@
+// 2. Ordenamiento por burbuja
+
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main() {
+	cout << "Esto aun no tiene el ejercicio, es solo la prueba" << endl;
+	system("pause");
+	return 0;
+}
